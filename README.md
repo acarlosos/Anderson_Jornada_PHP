af137875254c0ee198e0a6ca4e792cd49f8dce79
Antonio Carlos <antonio@MacBook-Pro-de-Antonio.local>
2021-02-16 07:55:47 -0300

    aula 4

---
+++ README.md
@@ -0,0 +1,1 @@
+# Anderson_Jornada_PHP
---
+++ aula4.php
@@ -0,0 +1,70 @@
+<?php
+
+
+$aula = new Aula4();
+
+$pessoas = [
+	38 => 'Antonio',
+	23 => 'Anderson',
+	65 => 'Andre',
+];
+
+$aula->podeIrWhile(10);
+
+class Aula4 
+{
+	public function podeIrWhile($idade = 1)
+	{
+		// ENQUANTO a idade for menor que 18 FAÇA.
+		while( $idade < 18 ) {
+			echo "Você não pode entrar na balada com " . $idade . " anos. <br>";
+			$idade++;
+		}
+		echo "Seja bem vindo ao role.";
+	}
+
+	public function podeIrDoWhile($idade = 1)
+	{
+		// FAÇA ENQUANTO a idade for menor que 18
+		
+		do {
+			echo "Você não pode entrar na balada com " . $idade . " anos. <br>";
+			$idade++;
+
+		}while($idade < 18);
+		echo "Seja bem vindo ao role.";
+
+	}
+
+	public function podeIrFor($x = 5)
+	{
+		//FAÇA X vezes
+		for($i=1 ; $i < $x ; $i++)
+		{
+			$this->podeIrWhile($i);
+			echo "<br>###################<br>";
+		}
+	}
+
+	
+	public function podeIrForeach($pessoas)
+	{
+		
+		// Percore itens de uma lista ou array
+		foreach ($pessoas as $idade => $nome) {
+			echo "$nome tem $idade anos de idade.<br>";
+		}
+
+
+	}
+
+	public function ler()
+	{
+		$nomes = file('teste.txt');
+		foreach ($nomes as $nome) {
+			echo "Bem vindo $nome.<br>";
+		}
+		
+	}
+	
+}
